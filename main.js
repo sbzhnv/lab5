b47b82bd704effca45411e419f77f1ca57db4bef
Guldana <122424443+sbzhnv@users.noreply.github.com>
2023-02-17 11:12:05 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
-function ()
+function load (){
 let column_1 = [5,9,2,10,7,4,8,5,10,5,10,2,7,1,8,8,9,6,10,4,8,4,3,4,2,9,2,9,6,4];
 let column_2 = [1,3,4,1,1,7,8,6,7,1,4,10,8,6,6,8,2,5,6,2,2,8,9,7,2,3,6,9,6,7];
 let column_3 = [82,59,64,64,87,44,76,52,62,100,43,41,51,45,56,82,64,89,75,93,69,94,83,70,46,53,83,88,85,89];
 let column_4 = [70,49,44,44,99,41,93,68,91,85,40,84,74,94,97,74,49,81,93,45,87,54,45,62,53,73,42,41,99,40];
 
 sum_1 = 0;
-for (i = 0; i<a.length; i++){
-  sum_1 = sum_1 + a[i];
+for (i = 0; i<column_1.length; i++){
+  sum_1 = sum_1 + column_1[i];
 }
 
 sum_2 = 0;
-for (i = 0; i<b.length; i++){
-  sum_2 = sum_2 + b[i];
+for (i = 0; i<column_2.length; i++){
+  sum_2 = sum_2 + column_2[i];
 }
 
 sum_3 = 0;
-for (i = 0; i<c.length; i++){
-  sum_3 = sum_3 + c[i];
+for (i = 0; i<column_3.length; i++){
+  sum_3 = sum_3 + column_3[i];
 }
 
 sum_4 = 0;
-for (i = 0; i<d.length; i++){
-  sum_4 = sum_4 + d[i];
+for (i = 0; i<column_4.length; i++){
+  sum_4 = sum_4 + column_4[i];
 }
 
-let sums = [sum_1, sum_2, sum_3, sum_4]
-const result = {
-  sumTotal : sums,
-  column_1 : a,
-  column_2 : b,
-  column_3 : c,
-  column_4 : d
+let sums = [sum_1, sum_2, sum_3, sum_4];
+let total_score = sum_1 + sum_2 + sum_3 + sum_4;
+
+const results = {
+  col1: [5,9,2,10,7,4,8,5,10,5,10,2,7,1,8,8,9,6,10,4,8,4,3,4,2,9,2,9,6,4],
+  col2: [1,3,4,1,1,7,8,6,7,1,4,10,8,6,6,8,2,5,6,2,2,8,9,7,2,3,6,9,6,7],
+  col3: [82,59,64,64,87,44,76,52,62,100,43,41,51,45,56,82,64,89,75,93,69,94,83,70,46,53,83,88,85,89],
+  col4: [70,49,44,44,99,41,93,68,91,85,40,84,74,94,97,74,49,81,93,45,87,54,45,62,53,73,42,41,99,40],
+  sums: [sum_1, sum_2, sum_3, sum_4],
+  total_score 
 }
 
-result.sumTotal
-console.log("Sums=" + result.sumTotal)
+console.log(results)
+
+}
